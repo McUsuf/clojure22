153a2f54336c928c6674da22bbe28e68e1afeac3
Виталий Вячеславович Ботвинко <v.botvinko@g.nsu.ru>
2022-10-02 15:29:48 +0700

    работает так же как и раньше, только разбил на большее число функций, бесполезно

--- src/clojure22_1/1.clj
+++ src/clojure22_1/1.clj
@@ -1,5 +1,36 @@
 (ns clojure22-1.1)
 
+(defn add-letters [word letters result]
+  (let [first_letter (first letters)
+        rest_letters (rest letters)
+        first_in_word (first word)]
+    (cond
+      (empty? letters) result
+      (= first_in_word first_letter) (add-letters word rest_letters result)
+      true (add-letters word rest_letters (cons (cons first_letter word) result))
+      )
+    )
+  )
+
+(defn add-to-words [words alphabet result]
+  (let [first_word (first words)
+        rest_words (rest words)]
+    (cond
+      (empty? words) result
+      (= (count first_word) 1) (add-to-words rest_words alphabet (cons (add-letters (list first_word) alphabet ()) result))
+      true (add-to-words rest_words alphabet (cons (add-letters first_word alphabet ()) result))
+      )
+    )
+  )
+
+(defn add-to-words-n [words alphabet n]
+  (cond
+    (empty? words) (add-to-words-n alphabet alphabet n)
+    (= n 1) words
+    true (add-to-words-n (add-to-words words alphabet ())  alphabet (- n 1))
+    )
+  )
+
 (defn task1-1-helper
   ([alphabet words words_copy result]
    (let [first_letter (first alphabet)
