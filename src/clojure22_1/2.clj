d94fd8e9efd707d8d357ce24c4f932551d85a31d
Виталий Вячеславович Ботвинко <vvboty@gmail.com>
2023-01-03 16:28:47 +0700

    update 1.2

--- src/clojure22_1/2.clj
+++ src/clojure22_1/2.clj
@@ -1,42 +1,27 @@
 (ns clojure22-1.2)
 
-(defn add-to-word
-  ([word alphabet] (add-to-word word alphabet ()))
-  ([word alphabet result]
-   (if (not-empty alphabet)
-     (let [current-element (first alphabet)
-           current-res (if (not= current-element (last word))
-                         (cons (concat word (list current-element))
-                               result)
-                         result)]
-       (recur word (rest alphabet) current-res))
-     result)
-   )
+(defn add-to-word [word alphabet]
+  (loop [alphabet alphabet result ()]
+    (if (not-empty alphabet)
+      (let [current-element (first alphabet)
+            current-res (if (not= current-element (first word))
+                          (cons (concat (list current-element) word)
+                                result)
+                          result)]
+        (recur (rest alphabet) current-res))
+      result)))
+
+(defn update-words [[words alphabet]]
+  (loop [words words result ()]
+    (if (not-empty words)
+      (recur (rest words)
+        (concat result (add-to-word (first words) alphabet)))
+      [result, alphabet])
+    )
   )
 
-(defn update-words
-  ([words alphabet] (update-words words alphabet ()))
-  ([words alphabet result]
-   (if (not-empty words)
-     (recur
-       (rest words)
-       alphabet
-       (concat result (add-to-word (first words) alphabet))
-       )
-     result)
-   )
-  )
-
-(defn permutations_rec
-  ([alphabet n]
-   (permutations_rec alphabet n (map #(list %) alphabet))
-   )
-  ([alphabet n result]
-   (if (= 1 n)
-     result
-     (recur alphabet (- n 1) (update-words result alphabet)))
-   )
-  )
+(defn perms_iter [n alphabet]
+  (first (nth (iterate update-words [`(()) alphabet]) n)))
 
 (defn -main []
-  (permutations_rec '("a" [1 2 3] ["e" "g"]) 3))
+  (perms_iter 3 '("a" [1 2 3] ["e" "g"])))
